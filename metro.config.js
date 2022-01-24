6363061a33c192b57c7694be9708039bbdf195f0
Emmanuel Yeboah <noelzappy@gmail.com>
2022-01-24 01:34:28 +0000

    Metro config #2 for EAS build

--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,22 @@
 // expo v40:
-const { getDefaultConfig } = require("@expo/metro-config");
-
-// expo v41: 
-// remove the @ (see: https://blog.expo.io/expo-sdk-41-12cc5232f2ef)
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require('expo/metro-config')
 
 module.exports = (async () => {
   const {
     transformer,
     resolver: { sourceExts, assetExts, ...resolver },
     ...config
-  } = await getDefaultConfig(__dirname);
+  } = await getDefaultConfig(__dirname)
   return {
-    ..config,
+    ...config,
     transformer: {
       ...transformer,
-      babelTransformerPath: require.resolve("react-native-svg-transformer")
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
       ...resolver,
-      assetExts: assetExts.filter(ext => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"]
-    }
-  };
-})();
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  }
+})()
